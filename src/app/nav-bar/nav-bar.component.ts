f23e5ac203491dbf01968add4dd45bb78265c530
Avi0520 <your-email@example.com>
2025-03-26 16:46:05 +0530

    update code

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -22,4 +22,12 @@ export class NavBarComponent {
     }, 100); // Small delay to ensure the DOM is ready
   }
 
+  closeMenu(): void {
+    const menuToggle = document.getElementById('menu-toggle') as HTMLInputElement;
+    if (menuToggle) {
+      menuToggle.checked = false;
+    }
+  }
+  
+
 }
